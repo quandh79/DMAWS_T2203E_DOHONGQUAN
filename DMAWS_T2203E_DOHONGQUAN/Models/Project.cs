65c676615d4943f66d690b0f12ea70168578b5d9
admin <dohongquantc@gmail.com>
2023-07-22 19:36:46 +0700

    first

---
+++ DMAWS_T2203E_DOHONGQUAN/Models/Project.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DMAWS_T2203E_DOHONGQUAN.Models
+{
+    [Table("Projects")]
+    public class Project
+    {
+        [Key]
+        public int ProjectId { get; set; }
+        [Required]
+        [StringLength(150, MinimumLength = 2)]
+        public string ProjectName { get; set; }
+        [Required]
+        public DateTime ProjectStartDate { get; set; }
+        public DateTime? ProjectEndDate { get; set; }
+        public virtual ICollection<ProjectEmployee> ProjectEmployees { get; set; }
+
+    }
+}
